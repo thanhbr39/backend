fe20a74d7e3661e5335949550ef9a1145623893c
thanhbr39 <thanhbr39@gmail.com>
2015-11-19 17:46:08 +0700

    change
    change

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,20 @@
  */
 
 module.exports = {
-	
+	// a CREATE action  
+	create: function(req, res, next) {
+
+		var params = req.params.all();
+
+		User.create(params, function(err, user) {
+
+			if (err) return next(err);
+
+			res.status(201);
+
+			res.json(user);
+
+		});
+	}
 };
 
